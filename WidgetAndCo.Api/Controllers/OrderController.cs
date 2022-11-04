c0cf2009e22bdfdd3eaf5073dcc07e22ffe4e919
UnordinaryCritter <112938051+UnordinaryCritter@users.noreply.github.com>
2022-11-04 21:22:26 +0100

    Extra files

---
+++ WidgetAndCo.Api/Controllers/OrderController.cs
@@ -0,0 +1,48 @@
+﻿using DomainLayer.Data;
+using DomainLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.ICustomServices;
+using System.Runtime.CompilerServices;
+
+namespace WidgetAndCo.Api.Controllers
+{
+    public class OrderController : ControllerBase
+    {
+        private readonly ICustomService<Order> _orderService;
+        private readonly ApplicationDbContext _applicationDbContext;
+        public OrderController(ICustomService<Order> orderService, ApplicationDbContext applicationDbContext)
+        {
+            _orderService = orderService;
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet(nameof(GetOrderById))]
+        public IActionResult GetOrderById(int Id)
+        {
+            var obj = _orderService.Get(Id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(obj);
+            }
+        }
+
+
+        [HttpGet(nameof(GetOrders))]
+        public IActionResult GetOrders(int Id)
+        {
+            var obj = _orderService.GetAll();
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(obj);
+            }
+        }
+    }
+}
---
+++ WidgetAndCo.Api/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using DomainLayer.Data;
+using DomainLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.CustomServices;
+using ServiceLayer.ICustomServices;
+
+namespace WidgetAndCo.Api.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ICustomService<Product> _productService;
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ProductController(ICustomService<Product> productService, ApplicationDbContext applicationDbContext)
+        {
+            _productService = productService;
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet(nameof(GetProductById))]
+        public IActionResult GetProductById(int Id)
+        {
+            var obj = _productService.Get(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(obj);
+            }
+        }
+
+
+        [HttpGet(nameof(GetProducts))]
+        public IActionResult GetProducts(int Id)
+        {
+            var obj = _productService.GetAll();
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(obj);
+            }
+        }
+    }
+}
